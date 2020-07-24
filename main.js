ee37a69736f0c166fc7754de47ac36ebeb1f21ac
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-07-24 10:14:18 -0700

    Added sliding animation to repos slides

--- main.js
+++ main.js
@@ -2,8 +2,16 @@ const repos = document.querySelectorAll(".repo");
 var currentRepoSlide = 0;
 
 function repoTransition(change) {
-	document.querySelector(".showrepo").classList.remove("showrepo");
+	let hiddenSlide = document.querySelector(".hiderepo");
+	if(hiddenSlide) hiddenSlide.classList.remove("hiderepo");
+
+	let shown = document.querySelector(".showrepo");
+	shown.classList.remove("showrepo");
+	shown.classList.add("hiderepo");
+
+
 	currentRepoSlide += change + repos.length;
 	currentRepoSlide %= repos.length;
+
 	repos[currentRepoSlide].classList.add("showrepo");
 }
