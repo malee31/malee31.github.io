8acf08853de175fb143d3154f32b11a89835f985
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-09-22 22:28:18 -0700

    Smoother slide in and out animations and logic for slideshow

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
 const repos = document.querySelectorAll(".repo");
+const reposize = repos.length;
 var currentRepoSlide = 0;
 
 function repoTransition(change) {
-	let hiddenSlide = document.querySelector(".hiderepo");
-	if(hiddenSlide) hiddenSlide.classList.remove("hiderepo");
+	let increment = change % reposize + reposize;
+
+	currentRepoSlide = (currentRepoSlide + increment) % reposize;
+
+	let left = repos[(currentRepoSlide + reposize - 1) % reposize];
+	let right = repos[(currentRepoSlide + 1) % reposize];
+	let show = repos[currentRepoSlide];
+
+	let hidden = document.querySelector(".hiderepo");
+	if(hidden) hidden.classList.remove("hiderepo");
 
 	let shown = document.querySelector(".showrepo");
 	shown.classList.remove("showrepo");
 	shown.classList.add("hiderepo");
 
+	if(change >= 1) {
+		let selected = document.querySelector(".leftrepo");
+		if(selected) selected.classList.remove("leftrepo");
+	} else if(change <= 1) {
+		let selected = document.querySelector(".rightrepo");
+		if(selected) selected.classList.remove("rightrepo");
+	}
 
-	currentRepoSlide += change + repos.length;
-	currentRepoSlide %= repos.length;
+	for(let item = 0; item < reposize; item++) {
+		if(item !== currentRepoSlide) {
+			repos[item].classList.remove("leftrepo");
+			repos[item].classList.remove("rightrepo");
+		}
+	}
 
-	repos[currentRepoSlide].classList.add("showrepo");
+	left.classList.add("leftrepo");
+	show.classList.add("showrepo");
+	right.classList.add("rightrepo");
 }
