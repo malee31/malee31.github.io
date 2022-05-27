aafe81f5c57b2338ab7ed1dadfd632d723122f65
Marvin Lee <43216761+malee31@users.noreply.github.com>
2022-05-27 12:52:06 -0700

    Converted entire site from normal HTML to Gatsby/React

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	siteMetadata: {
+		title: `Home`,
+		siteUrl: `https://malee31.github.io/home`
+	},
+	plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+		resolve: 'gatsby-plugin-manifest',
+		options: {
+			"icon": "src/images/favicons/android-chrome-512x512.png"
+		}
+	}, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+		resolve: 'gatsby-source-filesystem',
+		options: {
+			"name": "images",
+			"path": "./src/images/"
+		},
+		__key: "images"
+	}]
+};
