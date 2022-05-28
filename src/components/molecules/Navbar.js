b5ffb24a694d4096f93c096d55304ca3ec3d2ebc
Marvin Lee <43216761+malee31@users.noreply.github.com>
2022-05-28 10:29:05 -0700

    Split code into separate files

---
+++ src/components/molecules/Navbar.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+	return (
+		<>
+			<input id="menutoggle" className="hidden" type="checkbox"/>
+			<label id="menuicon" htmlFor="menutoggle">
+				<div id="menuiconcontainer">
+					<div className="menuicondiv"/>
+					<div className="menuicondiv"/>
+					<div className="menuicondiv"/>
+				</div>
+			</label>
+			<nav>
+				<div id="navpadding">
+					<a href="#home"><span>Home</span></a>
+					<a href="#about"><span>About Me</span></a>
+					<a href="#github"><span>Github</span></a>
+				</div>
+			</nav>
+		</>
+	);
+}
