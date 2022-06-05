113b488e6f0c53b03139a083a51bb7a926adb368
Marvin Lee <43216761+malee31@users.noreply.github.com>
2022-06-05 13:03:15 -0700

    Added underlay that can be clicked to dismiss navbar

--- src/components/molecules/Navbar.js
+++ src/components/molecules/Navbar.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 export default function Navbar() {
+	const [checked, setChecked] = useState(false);
+	const toggleCheck = () => setChecked(!checked);
+
 	return (
 		<>
-			<input id="menutoggle" className="hidden" type="checkbox"/>
+			<input id="menutoggle" className="hidden" type="checkbox" checked={checked} onChange={toggleCheck}/>
+			<div id="nav-underlay" onClick={() => setChecked(false)}/>
 			<label id="menuicon" htmlFor="menutoggle">
 				<div id="menuiconcontainer">
 					<div className="menuicondiv"/>
