918c1728321e3859e893a422fa0d43110fe3f59a
Marvin Lee <marvinlee.github@gmail.com>
2022-08-06 23:13:37 -0700

    Added many more projects onto featured

--- src/components/react/organisms/Featured.js
+++ src/components/react/organisms/Featured.js
@@ -6,6 +6,13 @@ import "./Featured.css";
 
 const FEATURED_DATA = [
 	{
+		title: "Pantry Cabinet",
+		description: "[WIP] A site for displaying what is available in your pantry and how many of each item you have",
+		repoLink: "https://github.com/malee31/the-pantry-cabinet",
+		demoLink: "https://pantry-cabinet.vercel.app/",
+		imageSrc: "repositories/PantryCabinet.png"
+	},
+	{
 		title: "CardinalHours App",
 		description: "An Expo App written in React Native for logging hours into the CardinalBotics CardinalHours server. (Demo password: bob)",
 		repoLink: "https://github.com/malee31/ReactNativeCardinalBoticsApp",
@@ -25,6 +32,48 @@ const FEATURED_DATA = [
 		repoLink: "https://github.com/malee31/Processing-Java-Projects/tree/Starfield",
 		demoLink: "https://malee31.github.io/Processing-Java-Projects/Starfield",
 		imageSrc: "repositories/Starfield.png"
+	},
+	{
+		title: "Slack to Discord Bridge",
+		description: "Creates a Discord archive by forwarding Slack messages and files to a Discord Server of your choosing",
+		repoLink: "https://github.com/malee31/Slack-to-Discord-Bridge",
+		demoLink: "https://malee31.github.io/Slack-to-Discord-Bridge",
+		imageSrc: "repositories/SlackToDiscordBridge.png"
+	},
+	{
+		title: "Canvas Helpers",
+		description: "Python CLI tool for automatically creating study groups on Canvas based on student preferences (See other contributors on GitHub)",
+		repoLink: "https://github.com/malee31/CanvasHelpers/tree/better-study-groups",
+		demoLink: "https://github.com/mfbutner/CanvasHelpers",
+		imageSrc: "repositories/CanvasHelpers.png"
+	},
+	{
+		title: "EduConnect",
+		description: "A chatroom created using WebSockets and Google Authentication that allows teachers to answer questions from students in real time!",
+		repoLink: "https://github.com/malee31/EduConnect",
+		demoLink: "https://educonnect-hyphen-hacks.herokuapp.com/chatroom/chatroom.html",
+		imageSrc: "repositories/EduConnect.png"
+	},
+	{
+		title: "Custom Form Maker",
+		description: "Build custom forms that forward data to a spreadsheet using the Google Sheets NodeJS API",
+		repoLink: "https://github.com/malee31/Custom-Form-Maker",
+		demoLink: "https://custom-google-form.herokuapp.com/",
+		imageSrc: "repositories/CustomFormMaker.png"
+	},
+	{
+		title: "Cube Color Picker",
+		description: "A color picker that uses WebGL to allow you to better choose from hues of the same color by presenting it as a lit cube",
+		repoLink: "https://github.com/malee31/CubeColorPicker",
+		demoLink: "https://malee31.github.io/CubeColorPicker",
+		imageSrc: "repositories/CubeColorPicker.png"
+	},
+	{
+		title: "Halloween Discord Bot",
+		description: "A Discord Bot I made for a one-off Halloween Event on a specific Discord Server!",
+		repoLink: "https://github.com/malee31/HalloweenDiscordBot",
+		demoLink: "https://github.com/malee31/HalloweenDiscordBot",
+		imageSrc: "repositories/HalloweenBot.png"
 	},
 	{
 		title: "Data Visualization",
