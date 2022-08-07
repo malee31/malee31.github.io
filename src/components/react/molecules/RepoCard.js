c759b9477c2dc8eb6750309119c03be8459afcdf
Marvin Lee <marvinlee.github@gmail.com>
2022-08-06 20:46:11 -0700

    Dependency update and repocard ARIA fix

--- src/components/react/molecules/RepoCard.js
+++ src/components/react/molecules/RepoCard.js
@@ -9,7 +9,7 @@ export default function RepoCard({ image, title, description, repoLink, demoLink
 	return (
 		<div
 			className={`repo-card-container ${flipped ? "repo-card-container-force-flip" : ""}`}
-			role="listitem"
+			role="menuitem"
 			tabIndex="0"
 			onClick={toggleFlip}
 			onKeyUp={({ code }) => {
@@ -17,10 +17,10 @@ export default function RepoCard({ image, title, description, repoLink, demoLink
 				toggleFlip();
 			}}
 		>
-			<div className="repo-card repo-card-front">
+			<div className="repo-card repo-card-front" aria-hidden={flipped}>
 				<GatsbyImage className="featured-card-img" alt={`${title} Demo Image`} image={image}/>
 			</div>
-			<div className="repo-card column-center repo-card-back">
+			<div className="repo-card column-center repo-card-back" aria-hidden={!flipped}>
 				<h3 className="repo-card-title">{title}</h3>
 				<p className="repo-card-description">{description}</p>
 				<div className="repo-card-links">
