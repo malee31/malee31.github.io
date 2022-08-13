ef278b3c78f73f145e7e625cc6f2910862fe78f6
Marvin Lee <marvinlee.github@gmail.com>
2022-08-12 20:33:12 -0700

    Manually color themed the repo cards

--- src/components/react/molecules/RepoCard.js
+++ src/components/react/molecules/RepoCard.js
@@ -2,7 +2,18 @@ import React, { useState } from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
 import "./RepoCard.css";
 
-export default function RepoCard({ image, title, description, repoLink, demoLink }) {
+export default function RepoCard(props) {
+	const {
+		image,
+		title,
+		description,
+		repoLink,
+		demoLink,
+		backgroundColor = "#AAAAAA",
+		textColor = "#191816",
+		...args
+	} = props;
+
 	const [flipped, setFlipped] = useState(false);
 	const toggleFlip = () => setFlipped(!flipped);
 
@@ -16,18 +27,32 @@ export default function RepoCard({ image, title, description, repoLink, demoLink
 				if(code !== "Space") return;
 				toggleFlip();
 			}}
+			{...args}
 		>
 			<div className="repo-card repo-card-front" aria-hidden={flipped}>
 				<GatsbyImage className="featured-card-img" alt={`${title} Demo Image`} image={image}/>
 			</div>
-			<div className="repo-card column-center repo-card-back" aria-hidden={!flipped}>
-				<h3 className="repo-card-title">{title}</h3>
-				<p className="repo-card-description">{description}</p>
-				<div className="repo-card-links">
-					{repoLink && <a href={repoLink} tabIndex={flipped ? 0 : -1}>Open in GitHub</a>}
-					{demoLink && <a href={demoLink} tabIndex={flipped ? 0 : -1}>Try Demo</a>}
+			<div
+				className="repo-card column-center repo-card-back"
+				aria-hidden={!flipped}
+			>
+				<h3
+					className="repo-card-title"
+					style={{ backgroundColor: backgroundColor, color: textColor }}
+				>
+					{title}
+				</h3>
+				<p className="repo-card-description">
+					{description}
+				</p>
+				<div
+					className="repo-card-links"
+					style={{ backgroundColor: backgroundColor, color: textColor }}
+				>
+					{repoLink && <a href={repoLink} tabIndex={flipped ? 0 : -1} style={{ color: textColor }}>Open in GitHub</a>}
+					{demoLink && <a href={demoLink} tabIndex={flipped ? 0 : -1} style={{ color: textColor }}>Try Demo</a>}
 				</div>
 			</div>
 		</div>
 	);
-}
+};
--- src/components/react/organisms/Featured.js
+++ src/components/react/organisms/Featured.js
@@ -10,73 +10,95 @@ const FEATURED_DATA = [
 		description: "[WIP] A site for displaying what is available in your pantry and how many of each item you have",
 		repoLink: "https://github.com/malee31/the-pantry-cabinet",
 		demoLink: "https://pantry-cabinet.vercel.app/",
-		imageSrc: "repositories/PantryCabinet.png"
+		imageSrc: "repositories/PantryCabinet.png",
+		backgroundColor: "#BA8C63",
+		textColor: "#342918"
 	},
 	{
 		title: "CardinalHours App",
 		description: "An Expo App written in React Native for logging hours into the CardinalBotics CardinalHours server. (Demo password: bob)",
 		repoLink: "https://github.com/malee31/ReactNativeCardinalBoticsApp",
 		demoLink: "https://malee31.github.io/ReactNativeCardinalBoticsApp",
-		imageSrc: "repositories/ReactNativeCardinalBoticsApp.png"
+		imageSrc: "repositories/ReactNativeCardinalBoticsApp.png",
+		backgroundColor: "#973337",
+		textColor: "#F5F5F5"
 	},
 	{
 		title: "Asteroids Game",
 		description: "A recreation of Asteroids from 1979! Controls are listed in the footer below so enjoy!",
 		repoLink: "https://github.com/malee31/Processing-Java-Projects/tree/AsteroidsGame",
 		demoLink: "https://malee31.github.io/Processing-Java-Projects/AsteroidsGame",
-		imageSrc: "repositories/AsteroidsGame.png"
+		imageSrc: "repositories/AsteroidsGame.png",
+		backgroundColor: "#262626",
+		textColor: "#969696"
 	},
 	{
 		title: "Starfield",
 		description: "A mesmerizing canvas with a calming sea of stars",
 		repoLink: "https://github.com/malee31/Processing-Java-Projects/tree/Starfield",
 		demoLink: "https://malee31.github.io/Processing-Java-Projects/Starfield",
-		imageSrc: "repositories/Starfield.png"
+		imageSrc: "repositories/Starfield.png",
+		backgroundColor: "#470B33",
+		textColor: "#EBEBEB"
 	},
 	{
 		title: "Slack to Discord Bridge",
 		description: "Creates a Discord archive by forwarding Slack messages and files to a Discord Server of your choosing",
 		repoLink: "https://github.com/malee31/Slack-to-Discord-Bridge",
-		imageSrc: "repositories/SlackToDiscordBridge.png"
+		imageSrc: "repositories/SlackToDiscordBridge.png",
+		backgroundColor: "#431F4F",
+		textColor: "#E8E8FD"
 	},
 	{
 		title: "Canvas Helpers",
 		description: "Python CLI tool for automatically creating study groups on Canvas based on student preferences (See other contributors on GitHub)",
 		repoLink: "https://github.com/malee31/CanvasHelpers/tree/better-study-groups",
-		imageSrc: "repositories/CanvasHelpers.png"
+		imageSrc: "repositories/CanvasHelpers.png",
+		backgroundColor: "#C55B43",
+		textColor: "#FFFFFF"
 	},
 	{
 		title: "EduConnect",
 		description: "A chatroom created using WebSockets and Google Authentication that allows teachers to answer questions from students in real time!",
 		repoLink: "https://github.com/malee31/EduConnect",
 		demoLink: "https://educonnect-hyphen-hacks.herokuapp.com/chatroom/chatroom.html",
-		imageSrc: "repositories/EduConnect.png"
+		imageSrc: "repositories/EduConnect.png",
+		backgroundColor: "#5175E8",
+		textColor: "#FFFFFF"
 	},
 	{
 		title: "Custom Form Maker",
 		description: "Build custom forms that forward data to a spreadsheet using the Google Sheets NodeJS API",
 		repoLink: "https://github.com/malee31/Custom-Form-Maker",
 		demoLink: "https://custom-google-form.herokuapp.com/",
-		imageSrc: "repositories/CustomFormMaker.png"
+		imageSrc: "repositories/CustomFormMaker.png",
+		backgroundColor: "#1786F1",
+		textColor: "#FFFFFF"
 	},
 	{
 		title: "Cube Color Picker",
 		description: "A color picker that uses WebGL to allow you to better choose from hues of the same color by presenting it as a lit cube",
 		demoLink: "https://malee31.github.io/CubeColorPicker",
-		imageSrc: "repositories/CubeColorPicker.png"
+		imageSrc: "repositories/CubeColorPicker.png",
+		backgroundColor: "#3D93E7",
+		textColor: "#E9EDFA"
 	},
 	{
 		title: "Halloween Discord Bot",
 		description: "A Discord Bot I made for a one-off Halloween Event on a specific Discord Server!",
 		repoLink: "https://github.com/malee31/HalloweenDiscordBot",
-		imageSrc: "repositories/HalloweenBot.png"
+		imageSrc: "repositories/HalloweenBot.png",
+		backgroundColor: "#303135",
+		textColor: "#E2E3E5"
 	},
 	{
 		title: "Data Visualization",
 		description: "A nice animated chart for random data that can be changed and adapted for different data sets",
 		repoLink: "https://github.com/malee31/Processing-Java-Projects/tree/DataVisualization",
 		demoLink: "https://malee31.github.io/Processing-Java-Projects/DataVisualization",
-		imageSrc: "repositories/DataVisualization.png"
+		imageSrc: "repositories/DataVisualization.png",
+		backgroundColor: "#646464",
+		textColor: "#FFFFFF"
 	}
 ];
 export default function Experimental() {
