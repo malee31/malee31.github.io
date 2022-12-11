93e1acc606769e5b6ebc3b06c4505cf5899c9168
Marvin Lee <marvinlee.github@gmail.com>
2022-12-10 23:54:06 -0800

    Beginning to customize 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,18 @@
-import React from "react"
+import React from "react";
 import { Link } from "gatsby"
+import "../styles.css";
+import Seo from "../components/react/atoms/SEO";
+import "./404.css";
 
-// styles
-const pageStyles = {
-	color: "#232129",
-	padding: "96px",
-	fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-	marginTop: 0,
-	marginBottom: 64,
-	maxWidth: 320,
-}
+export const Head = () => <Seo siteTitle="Not Found"/>;
 
-const paragraphStyles = {
-	marginBottom: 48,
-}
-const codeStyles = {
-	color: "#8A6534",
-	padding: 4,
-	backgroundColor: "#FFF4DB",
-	fontSize: "1.25rem",
-	borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
+export default function NotFoundPage() {
 	return (
-		<main style={pageStyles}>
-			<title>Not found</title>
-			<h1 style={headingStyles}>Page not found</h1>
-			<p style={paragraphStyles}>
-				Sorry{" "}
-				<span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-				we couldn’t find what you were looking for.
-				<br/>
-				{process.env.NODE_ENV === "development" ? (
-					<>
-						<br/>
-						Try creating a page in <code style={codeStyles}>src/pages/</code>.
-						<br/>
-					</>
-				) : null}
-				<br/>
-				<Link to="/">Go home</Link>.
-			</p>
+		<main>
+			<h1>There's Nothing Here!</h1>
+			<Link to="/">
+				(Go back to home)
+			</Link>.
 		</main>
 	);
 }
-
-export default NotFoundPage
