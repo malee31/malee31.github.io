923f9bbcc9d88bd16fd783be389f0c8ef5b86c3c
Marvin Lee <marvinlee.github@gmail.com>
2023-01-18 13:14:39 -0800

    Added trace of first frame of cube to show before the canvas loads

--- src/components/react/organisms/Splash.js
+++ src/components/react/organisms/Splash.js
@@ -1,15 +1,30 @@
 import React, { Suspense } from "react";
 import ScreenSection from "../atoms/FullScreenSection";
 import ScrollBanner from "../atoms/ScrollBanner";
+import CubeTrace from "../../../images/cube-trace.svg";
 import "./Splash.css";
 
 const ThreeSplash = React.lazy(() => import("../../three/organisms/ThreeSplash"));
+
+// Hard-coded inline positioning for the first frame of the splash (traced)
+function SubstituteSplash() {
+	return (
+		<div style={{ position: "absolute", width: "100%", height: "100%" }}>
+			<img
+				style={{ position: "relative", left: "-6px", width: "316px", height: "100%" }}
+				src={CubeTrace}
+				alt="Cube"
+				role="presentation"
+			/>
+		</div>
+	);
+}
 
 export default function SplashV2() {
 	return (
 		<ScreenSection id="home" natural={true} className="column-center column-align">
 			<SplashOverlay>
-				<Suspense fallback={null}>
+				<Suspense fallback={<SubstituteSplash/>}>
 					<ThreeSplash fallback={null}/>
 				</Suspense>
 			</SplashOverlay>
@@ -20,7 +35,7 @@ export default function SplashV2() {
 	);
 }
 
-function SplashOverlay({children}) {
+function SplashOverlay({ children }) {
 	return (
 		<div className="splash-overlay">
 			<div className="splash-overlay-content">
