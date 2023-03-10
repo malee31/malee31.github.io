7f2cb49e2b6fae7fc3bb70012194a92bfeeefb33
Marvin Lee <marvinlee.github@gmail.com>
2023-03-10 15:11:53 -0800

    Rudimentary easing into animation (Slight jump between animations)

--- src/components/three/molecules/ThreeFigure.js
+++ src/components/three/molecules/ThreeFigure.js
@@ -1,24 +1,41 @@
 import React, { useRef } from "react";
 import { Box, PerspectiveCamera, TrackballControls } from "@react-three/drei";
 import GrowingBox from "../atoms/ThreeGrowingBox";
-import { animated, useSpring } from "@react-spring/three";
+import { animated, easings, useSpring } from "@react-spring/three";
 
 export default function ThreeFigure({ hideSubstitute }) {
 	const camRef = useRef();
-	const groupRef = useRef();
+	const transitionTime = 20000;
 
-	const { rotateX, rotateY } = useSpring({
+	const accelerate = useSpring({
 		from: {
 			rotateX: Math.PI / 6,
 			rotateY: Math.PI / 6,
 		},
 		to: {
-			rotateX: 2 * Math.PI + Math.PI / 6,
-			rotateY: 10 * Math.PI + Math.PI / 6
+			rotateX: 2 * Math.PI,
+			rotateY: 10 * Math.PI,
+		},
+		config: {
+			duration: transitionTime,
+			easing: easings.easeInSine
+		},
+		loop: false
+	});
+	const velocity = useSpring({
+		from: {
+			rotateX: 0,
+			rotateY: 0,
+		},
+		to: {
+			rotateX: 2 * Math.PI,
+			rotateY: 10 * Math.PI,
 		},
 		config: {
 			duration: 20000,
 		},
+		delay: transitionTime,
+		reset: true,
 		loop: true
 	});
 
@@ -33,35 +50,40 @@ export default function ThreeFigure({ hideSubstitute }) {
 				rotateSpeed={2.5}
 			/>
 			<animated.group
-				ref={groupRef}
 				position={[0, 0, 0]}
-				rotation-x={rotateX}
-				rotation-y={rotateY}
+				rotation-x={accelerate.rotateX}
+				rotation-y={accelerate.rotateY}
 			>
-				<GrowingBox
-					color="#067DDC"
-					position={[0, 3, 0]}
-					args={[12, 6, 12]}
-					scale={[0.25, 0.3, 1]}
-					scaleRate={[0.01, 0, 0]}
-				/>
-				<Box
+				<animated.group
 					position={[0, 0, 0]}
-					args={[10, 9.95, 10]}
+					rotation-x={velocity.rotateX}
+					rotation-y={velocity.rotateY}
 				>
-					<meshPhongMaterial
-						color="#EEEEEE"
-						wireframe={false}
-						onBeforeCompile={hideSubstitute}
+					<GrowingBox
+						color="#067DDC"
+						position={[0, 3, 0]}
+						args={[12, 6, 12]}
+						scale={[0.25, 0.3, 1]}
+						scaleRate={[0.01, 0, 0]}
 					/>
-				</Box>
-				<GrowingBox
-					color="#067DDC"
-					position={[0, -3, 0]}
-					args={[12, 6, 12]}
-					scale={[1, 0.3, 0.25]}
-					scaleRate={[0, 0, 0.01]}
-				/>
+					<Box
+						position={[0, 0, 0]}
+						args={[10, 9.95, 10]}
+					>
+						<meshPhongMaterial
+							color="#EEEEEE"
+							wireframe={false}
+							onBeforeCompile={hideSubstitute}
+						/>
+					</Box>
+					<GrowingBox
+						color="#067DDC"
+						position={[0, -3, 0]}
+						args={[12, 6, 12]}
+						scale={[1, 0.3, 0.25]}
+						scaleRate={[0, 0, 0.01]}
+					/>
+				</animated.group>
 			</animated.group>
 		</>
 	);
